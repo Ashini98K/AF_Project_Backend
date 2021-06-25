b303a1a23c5c1b8f47592b06bd07ab00f43079a6
Ashini98K <67262488+Ashini98K@users.noreply.github.com>
2021-06-25 15:31:09 +0530

    Change done to user model

--- IT19136134/models/user-model.js
+++ IT19136134/models/user-model.js
@@ -6,7 +6,7 @@ const UserModel = new mongoose.Schema({
   email: { type: String, required: true, trim: true },
   contactNo: { type: String, required: true },
   password: { type: String, required: true, trim: true },
-  type: { type: String, required: true, trim: true },
+  type: { type: String, trim: true },
 });
 
 module.exports = mongoose.model("users", UserModel);
